996eeb530fc9f52308bf642d33c268ccbaf674b1
Divyam Singh <divyamsingh234@gmail.com>
2017-07-07 21:48:36 +0530

    Add files via upload

---
+++ ppafix.sh
@@ -0,0 +1,4 @@
+wget http://blog.anantshri.info/content/uploads/2010/09/add-apt-repository.sh.txt
+cp add-apt-repository.sh.txt /usr/sbin/add-apt-repository
+chmod o+x /usr/sbin/add-apt-repository
+chown root:root /usr/sbin/add-apt-repository
